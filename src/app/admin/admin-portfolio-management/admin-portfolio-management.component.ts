f0d5400a0841d736f9197a39d37b47e281fdcbe3
keple <smw195@gmail.com>
2022-09-17 18:21:40 +0000

    message 알림 영역 / 사이드 프로젝트 화면 수정

--- src/app/admin/admin-portfolio-management/admin-portfolio-management.component.ts
+++ src/app/admin/admin-portfolio-management/admin-portfolio-management.component.ts
@@ -10,10 +10,20 @@ import {AdminServicesModel} from '../model/admin.services.model';
 })
 export class AdminPortfolioManagementComponent implements OnInit {
   public portFolioList: Array<AdminServicesModel>;
+  public displayedColumns: Array<string>;
   constructor(private portfolioService: PortfolioService) { }
 
   ngOnInit(): void {
+    this.displayedColumns = ['serviceName' , 'serviceActuatorUrl' , 'tech', 'serviceRouteUrl' , 'updateButton', 'deleteButton'];
     this.portfolioService.getPortfolioList().then(data => this.portFolioList = data);
+  }
+  update(serviceName: string): void{
+    // test
+    console.log(serviceName);
+  }
+  delete(serviceName: string): void {
+    // test
+    console.log(serviceName);
   }
 
 }
